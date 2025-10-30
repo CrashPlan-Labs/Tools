86cb18900b46e8f727c31e800ac40e4481a38dc8
Dan Schmidt <dan.schmidt@crashplan.com>
2025-10-30 16:08:16 -0500

    Refactored archive key password/custom key and cleaned up verbiage.

--- push_restore.ps1
+++ push_restore.ps1
@@ -1,29 +1,26 @@
 param(
-    [Parameter(Mandatory=$true, HelpMessage="Enter the username with push restore permission to run this with")]
+    [Parameter(Mandatory=$true, HelpMessage="Username with push restore permissions")]
     [string]$User,
-    [Parameter(Mandatory=$true, HelpMessage="Enter the base URL to run the script with")]
+    [Parameter(Mandatory=$true, HelpMessage="Base URL (e.g. https://console.us2.crashplan.com)")]
     [string]$BaseUrl,
     [Parameter(Mandatory=$true, HelpMessage="Source device GUID")]
     [string]$SourceComputerGuid,
     [Parameter(Mandatory=$true, HelpMessage="Target device GUID")]
     [string]$TargetComputerGuid,
-    [Parameter(Mandatory=$false, HelpMessage="Input file, list of paths to restore. These can be files, directories, path separators can be / or \. If you want to do a full restore for a system this will assume C:/ for Windows and / for macOS and Linux")]
+    [Parameter(Mandatory=$false, HelpMessage="Input file containing a list of paths to restore. These can be files or directories. Path separators can be / or \. If you want to do a full restore for a system this will assume C:/ for Windows and / for macOS and Linux.")]
     [string]$InputFile,
-    [Parameter(Mandatory=$false, HelpMessage="Target location that exists on disk for the files to be restored (C:/pushrestore/SourceComputerGuid is default if target is Windows, /pushrestore/SourceComputerGuid if macOS/Linux, and use / instead of \)")]
+    [Parameter(Mandatory=$false, HelpMessage="Target location that exists on disk for the files to be restored (C:/pushrestore/SourceComputerGuid is default if target is Windows, /pushrestore/SourceComputerGuid if macOS/Linux, and use / instead of \).")]
     [string]$TargetDirectory,
-    [Parameter(Mandatory=$false, HelpMessage="Date in a yyyy-mm-dd format to restore from, if not provided the latest version will be restored")]
+    [Parameter(Mandatory=$false, HelpMessage="Date in a yyyy-mm-dd format to restore from. If none is provided the latest version will be restored.")]
     [string]$RestoreDate,
-    [Parameter(Mandatory=$false, HelpMessage="Add paramater to restore deleted files, false by default")]
+    [Parameter(Mandatory=$false, HelpMessage="Add paramater to restore deleted files. Disabled by default.")]
     [switch]$RestoreDeletedFiles
 )
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-if ($BaseUrl -eq "") {
-    $BaseUrl = Read-Host -Prompt "Please provide the base URL for API requests, e.g., https://console.us2.crashplan.com"
-}
 if ($TargetDirectory -eq "") {
-    $TargetDirectory = Read-Host -Prompt "What is the target location that exists on disk for the files to be restored? If nothing is passed in C:/pushrestore/$SourceComputerGuid/ or /pushrestore/$SourceComputerGuid/ will be used. (use / instead of \)"
+    $TargetDirectory = Read-Host -Prompt "What is the target location that exists on disk for the files to be restored? If not provided C:/pushrestore/$SourceComputerGuid/ or /pushrestore/$SourceComputerGuid/ will be used. (use / instead of \)."
 }
 Write-Host "RestoreDeletedFiles is set to $RestoreDeletedFiles"
 $UserAgent = "PushRestoreScript"
@@ -31,15 +28,15 @@ $pushRestorePathsAtATime = 10000
 
 $credentials = Get-Credential -Credential $User
 
-$PrivatePassword = Read-Host -AsSecureString -Prompt "Archive key password. Press Enter to skip if not required"
-$CustomKey = Read-Host -AsSecureString -Prompt "Custom key. Press Enter to skip if not required"
+$PrivatePassword = Read-Host -AsSecureString -Prompt "Archive key password. Press Enter to skip if not required."
+$CustomKey = Read-Host -AsSecureString -Prompt "Custom key. Press Enter to skip if not required."
 
 # Get an authorization token for the rest of the API calls, prompting for a two-factor code as needed. Works with PowerShell 5.1 and PowerShell Core
 $pair = "$($credentials.username):$($credentials.GetNetworkCredential().Password)"
 $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
 $basicAuthValue = "Basic $encodedCreds"
 
-$totp = Read-Host "Enter your 2-factor code. Press Enter to skip if not required"
+$totp = Read-Host "Enter your 2-factor code. Press Enter to skip if not required."
 if ([string]::IsNullOrEmpty($totp)) {
     $basicAuthHeaders = @{
         Authorization = $basicAuthValue
@@ -54,10 +51,10 @@ if ([string]::IsNullOrEmpty($totp)) {
 $login_url = $BaseUrl + '/api/v3/auth/jwt?useBody=true'
 $token = (Invoke-RestMethod -Uri $login_url -Method Get  -Headers $basicAuthHeaders -ContentType 'application/json' -SessionVariable session -UserAgent $userAgent).data.v3_user_token
 if($token){
-    write-host "we are Authorized, continuing"
+    write-host "We are authorized - continuing"
 }
 else {
-    Write-Host "Invalid username, password, one time code, or base url. Exiting please try again."
+    Write-Host "Invalid username, password, 2-factor code, or base URL. Please rerun the script and try again."
     exit
 }
 
@@ -121,10 +118,10 @@ Write-Host "Target Computer information: Username:" $targetUser.data.username "O
 if ($InputFile) {
     Write-Host "Restoring files or paths listed in $InputFile."
 } else {
-    Write-Host "Restoring $defaultPath for $SourceComputerGuid. From $RestoreDate."
+    Write-Host "Restoring $defaultPath."
 }
 
-Write-Host "Files will go here on the target device: $TargetDirectory"
+Write-Host "Target device restore direcotry: $TargetDirectory"
 
 $confirmation = Read-Host "If the above information is correct, enter Y/y to continue. Press any other key to exit."
 
@@ -140,8 +137,28 @@ $DataKeyTokenPostValues = @{
     computerGuid = $SourceComputerGuid
 }
 
+# Function to convert SecureStrings back to Strings
+# Checks if empty first, and if so, returns a blank string
+# PowerShell Core and Desktop have two mutually exclusive ways of converting SecureStrings to Strings
+function convertString($SecureString) {
+    if ($SecureString.Length -eq 0) {
+        $OutString = ""
+    } else {
+        if ($PSVersionTable.PSEdition -eq "Desktop") {
+            $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
+            $OutString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
+        } else {
+            $OutString = $SecureString | ConvertFrom-SecureString -AsPlainText
+        }
+    } 
+    return $OutString
+}
+
+$PrivatePassString = convertString $PrivatePassword
+$CustomKeyString = convertString $CustomKey
 # Define the push restore function
-function pushRestore($inputPaths, $SourceComputerGuid, $TargetComputerGuid, $TargetDirectory, $ServerGUID) {
+function pushRestore($inputPaths, $SourceComputerGuid, $TargetComputerGuid, $TargetDirectory, $ServerGUID, $PrivatePassString, $CustomKeyString) {
     Write-Host "$(Get-Date): Kicking off restore of provided paths."
     # Initialize an array to hold the JSON objects
     $jsonArray = @()
@@ -166,37 +183,10 @@ function pushRestore($inputPaths, $SourceComputerGuid, $TargetComputerGuid, $Tar
 
     $DataKeyToken = (Invoke-RestMethod -Headers $headers -Uri "$BaseUrl/api/DataKeyToken" -Method Post -Body (ConvertTo-Json $DataKeyTokenPostValues) -ContentType application/json -WebSession $session -UserAgent $UserAgent).data.dataKeyToken
 
-    #only convert PrivatePassword and CustomKey to strings if they are not empty, otherwise PS errors out
-    #need to use older string conversion methods for pre-7.0 releases of PS
-    if ($PrivatePassword.Length -eq 0) {
-        $PrivatePasswordString = ""
-    } else {
-        if ($PSVersionTable.PSEdition -eq "Desktop") {
-            $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($PrivatePassword)
-            $PrivatePasswordString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
-        } else {
-            $PrivatePasswordString = $PrivatePassword | ConvertFrom-SecureString -AsPlainText
-        }
-    }
-
-    if ($CustomKey.Length -eq 0) {
-        $CustomKeyString = ""
-    } else {
-        if ($PSVersionTable.PSEdition -eq "Desktop") {
-            $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($CustomKey)
-            $CustomKeyString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
-        } else {
-            $CustomKeyString = $CustomKey | ConvertFrom-SecureString -AsPlainText
-        }
-
-    }
-
     $WebRestorePostValues = @{
         computerGuid = $SourceComputerGUID
         dataKeyToken = $DataKeyToken
-        privatePassword = $PrivatePasswordString
+        privatePassword = $PrivatePassString
         encryptionKey = $CustomKeyString
     }
     $body = ConvertTo-Json $WebRestorePostValues
@@ -229,9 +219,9 @@ function pushRestore($inputPaths, $SourceComputerGuid, $TargetComputerGuid, $Tar
 # Read all lines from the input file into the pushRestore array, and call the pushRestore function for each group of paths. If the input file does not exist, dynamically choose the path based on whether it's a Windows or macOS system. Assuming Windows by default.
 
 if (Test-Path -Path $InputFile) {
-    Get-Content -Path $InputFile -ReadCount $pushRestorePathsAtATime | ForEach-Object { pushRestore $_ $SourceComputerGuid $TargetComputerGuid $TargetDirectory $ServerGUID }
+    Get-Content -Path $InputFile -ReadCount $pushRestorePathsAtATime | ForEach-Object { pushRestore $_ $SourceComputerGuid $TargetComputerGuid $TargetDirectory $ServerGUID $PrivatePassString $CustomKeyString }
 } else {
-    Write-Host "Input file $InputFile does not exist, restoring the full drive for $SourceComputerGuid."
-    pushRestore $defaultPath $SourceComputerGuid $TargetComputerGuid $TargetDirectory $ServerGUID
+    Write-Host "Input file $InputFile does not exist. Restoring the full drive for $SourceComputerGuid."
+    pushRestore $defaultPath $SourceComputerGuid $TargetComputerGuid $TargetDirectory $ServerGUID $PrivatePassString $CustomKeyString
     exit
 }
